e0977efa1985ce167fda6e6b3d013a2f7e0b0e07
Zahra MohammadPour <zahramohammadpour497@gmail.com>
2023-02-09 22:25:39 +0330

    Project Refactor, switch to next.js, mongoDB added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,34 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+const morgan = require('morgan')
+import {readdirSync} from 'fs'
+require('dotenv').config()
+
+//create express app
 const app = express()
-app.get("/api", (req, res)=>{
-    res.json({ "users":["1","2","3"] })
+
+//db connection
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
 })
-app.listen(5000,() => { console.log("server started on port 5000") })
+    .then(() => console.log("**DB CONNECTED**"))
+    .catch((err) => console.log("DB CONNECTION ERR => ", err))
+
+//apply middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+// app.use((req,res,next)=>{
+//     console.log("my own middleware")
+//     next()
+// })
+
+//rout
+readdirSync('./routes').map((r)=>{
+    app.use('/api', require(`./routes/${r}`))
+})
+
+//port
+const port = process.env.PORT || 5000
+app.listen(port,() => { console.log("server started on port 5000") })
